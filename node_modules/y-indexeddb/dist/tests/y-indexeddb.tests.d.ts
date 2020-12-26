56a63cb8a95cc5e648426c8cdea44c8ab8ef6956
Frank Pagan <frank@cocreate.app>
2020-12-26 00:22:20 +0000

    remove node modules

--- node_modules/y-indexeddb/dist/tests/y-indexeddb.tests.d.ts
+++
@@ -1,5 +0,0 @@
-export function testIdbUpdateAndMerge(tc: t.TestCase): Promise<void>;
-export function testIdbConcurrentMerge(tc: t.TestCase): Promise<void>;
-export function testMetaStorage(tc: t.TestCase): Promise<void>;
-import * as t from "lib0/testing";
-//# sourceMappingURL=y-indexeddb.tests.d.ts.map
