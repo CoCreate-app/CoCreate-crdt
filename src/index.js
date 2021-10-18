be90e1d33a5350ae12c0aeca532250dc2b01f76d
Frank Pagan <frank@cocreate.app>
2021-10-18 22:00:27 +0000

    feat: crdtDoc made from javascript map

--- src/index.js
+++ src/index.js
@@ -1,443 +1,361 @@
 /*globals config, atob, btoa, CustomEvent*/
-
-import * as Y from 'yjs';
-import { WebsocketProvider } from 'y-websocket';
-// import { IndexeddbPersistence } from 'y-indexeddb';
 import crud from '@cocreate/crud-client';
+import message from '@cocreate/message-client';
 
+const docs = new Map();
 
-class CoCreateCRDTClass {
-	
-	constructor(org, ydoc) {
-		this.org = config.organization_Id;
-		this.doc = ydoc;
-		this.docs = {};
-	}
-
-
-	/*
-	crdt.init({
-		collection: "module",
-		document_id: "",
-		name: "",
-		metadata: "xxxx"
-	})
-	*/
-	init(info) {
-		try {
-			this.createDoc(info);
-		} catch(e) {
-			console.log('Invalid param', e);
-		}
-	}
-	
-	
-	createDoc(info) {
-
-		let docName = this.generateDocName(info);
-		let typeName = this.generateTypeName(info);
-
-		if (this.docs[docName]) { 
-			if (!this.docs[docName].types.some((type) => type === typeName)) {
-				this.docs[docName].types.push(typeName);
-				this._registerUpdateEvent(this.docs[docName], typeName);
-			}
-			return false;
-		} 
-		
-		const crdtDoc = this.doc;
-		const url_socket = this.__getSocketUrl();
-		const sharedType = crdtDoc.getText(typeName);
-
-		var socketProvider = new WebsocketProvider(url_socket, docName, crdtDoc);
-		
-		// const indexeddbProvider = new IndexeddbPersistence(docName, crdtDoc);
-		// indexeddbProvider.on('synced', () => {});
-
-		const awareness = socketProvider.awareness;
-
-		this._cursors = new Map();
-		
-		this._awarenessListener = event => {
-		 const f = clientId => {
-			  this.updateSelection(crdtDoc, typeName, sharedType, this._cursors, clientId, awareness);
-		  };
-		  event.added.forEach(f);
-		  event.removed.forEach(f);
-		  event.updated.forEach(f);
-		};
-		
-		awareness.on('update', this._awarenessListener);
-
-        this.docs[docName] = {
-			id: docName,
-			doc: crdtDoc,
-			socket: socketProvider,
-			awareness: awareness,
-			types: [typeName],
-			// indexeddb: indexeddbProvider
-		};
-		this._registerUpdateEvent(this.docs[docName], typeName);
-
-		return true;
-	}
-	
-	__getSocketUrl() {
-		let w_location = window.location || window.parent.location;
-		let w_protocol = w_location.protocol;
-		let w_host = w_location.host;
-		if (w_location.protocol === "about:") {
-			w_protocol = w_location.protocol;
-			w_host = document.referrer;
-		}
-		let protocol = w_protocol === 'http:' ? 'ws' : 'wss';
-
-		let url_socket = `${protocol}://${w_host}:8080/`;
-		if (window.config && window.config.host) {
-			if (window.config.host.includes("://")) {
-				url_socket = `${window.config.host}/`;
-			} else {
-				url_socket = `${protocol}://${window.config.host}/`;
-			}
-		}
-		
-		url_socket += "crdt/";
-		
-		return url_socket;
-	}
-	
-	_registerUpdateEvent(docName, typeName) {
-		const crdtDoc = docName.doc;
-		const sharedType = crdtDoc.getText(typeName);
-		let self = this;
-		
-		sharedType.observe((event) => {
-			self._crdtUpdateEvent(event, typeName);
-		});
-	}
-
-	_crdtUpdateEvent(event, typeName) {
-		const eventDelta = event.delta;
-		if (eventDelta.length == 0)	return;
-		
-		const info = JSON.parse(atob(typeName));
-		// let is_save_value = false
-		
-		const update_event = new CustomEvent('cocreate-crdt-update', {
-			detail: {eventDelta,...info}, 
-		});
-		
-		window.dispatchEvent(update_event);
-	}
-	
-	sendPosition(info) {
-		let docName = this.generateDocName(info);
-		let typeName = this.generateTypeName(info);
-		let type = this.docs[docName].doc.getText(typeName);
-		if (!type) return;
-		if (info.start != null && info.end != null){
-			var anchor = Y.createRelativePositionFromTypeIndex(type, info.start);
-			var head = Y.createRelativePositionFromTypeIndex(type, info.end);
-			
-			this.docs[docName].socket.awareness.setLocalStateField('cursor', {anchor, head});
-		}
-		else {
-			this.docs[docName].socket.awareness.setLocalStateField('cursor', null);
-		}
-			
-	}
-	
-	updateSelection (y, cm, type, cursors, clientId, awareness)  {
-
-		if(clientId !== this.doc.clientID){
-			
-			const m = cursors.get(clientId);
-				if (m !== undefined) {
-					m.caret.clear();
-				if (m.sel !== null) {
-				  m.sel.clear();
-				}
-				cursors.delete(clientId);
-			}
-			  
-			const aw = awareness.getStates().get(clientId);
-	
-			if (aw === undefined) {
-				this.removeCursor(clientId, aw);
-				return;
-			}
-			const user = aw.user || {};
-			if (user.color == null) {
-				user.color = '#ffa500';
-			}
-			if (user.name == null) {
-				user.name = `User: ${clientId}`;
-			}
-			const cursor = aw.cursor;
-			if (cursor == null || cursor.anchor == null || cursor.head == null) {
-				this.removeCursor(clientId, aw);
-				return;
-			}
-			// const start = cursor.anchor.item.clock;
-			// const end = cursor.head.item.clock;
-
-			const anchor = Y.createAbsolutePositionFromRelativePosition(Y.createRelativePositionFromJSON(cursor.anchor), y);
-			const head = Y.createAbsolutePositionFromRelativePosition(Y.createRelativePositionFromJSON(cursor.head), y);
-			if (anchor !== null && head !== null ) {
-				let info = this.parseName(cursor.anchor['tname']);
-				
-				let selection = {
-					collection: info.collection,
-					document_id: info.document_id,
-					name: info.name,
-					start: anchor.index,
-					end: head.index,
-					clientId: clientId,
-					user:{
-						'color':user.color,
-						'name':user.name
-					},
-				};
-				const cursorUpdate = new CustomEvent('updateCursor', {
-					detail: {selection}, 
-				});
-				
-				window.dispatchEvent(cursorUpdate);
-			}
-		}
-	}
-	
-	removeCursor(clientId, aw){
-		const cursorRemove = new CustomEvent('removeCursor', {
-			detail: {clientId, aw}, 
-		});
-		window.dispatchEvent(cursorRemove);
-		return;
-
-	}
-	
-	deleteDoc(docName) {
-		if (this.docs[docName]) {
-			delete this.docs[docName];
-		}
-	}
-	
-	destroyObserver(docName, typeName) {
-		this.docs[docName].doc.getText(typeName).unobserve((event) => {});
-		this.docs[docName].socket.awareness.off('change', this._awarenessListener);
-	}
-	
-	parseName(id) {
-		let data = JSON.parse(atob(id));
-		let name = {org: data.org, collection: data.collection, document_id: data.document_id};
-		return {
-			id: btoa(JSON.stringify(name)),
-			collection: data.collection,
-			document_id: data.document_id,
-			name: data.name
-		};
-	}
-	
-	generateDocName(info) {
-		let docName = {org: config.organization_Id, collection: info.collection, document_id: info.document_id};
-		return btoa(JSON.stringify(docName));        
-	}
-	
-	generateTypeName(info) {
-		let nameId = {org: config.organization_Id, collection: info.collection, document_id: info.document_id, name: info.name};
-		return btoa(JSON.stringify(nameId));        
-	}
-
-	/*
-	crdt.replaceText({
-		collection: "module",
-		document_id: "",
-		name: "",
-		value: "",
-		updateCrud: true | false,
-		element: dom_object,
-		metadata: "xxxx"
-	})
-	*/
-	replaceText(info){
-		let docName = this.generateDocName(info);
-		let typeName = this.generateTypeName(info);
-		if (!this.docs[docName]){
-			this.init(info);
-		}
-		if (info.updateCrud != false) info.updateCrud = true;
-		
+function init(info) {
+	try {
+		var docName = generateDocName(info);
 		if (docName) {
-			let oldData = this.docs[docName].doc.getText(typeName).toString();
-			let textValue = info['value'].toString();
-			if (oldData && oldData.length > 0) {
-				this.deleteText({collection: info.collection, document_id: info.document_id, name: info.name, position: 0, length: Math.max(oldData.length, textValue.length), crud: info['crud']});
+			if (!docs.has(docName)) {
+				let doc; // getDoc from db
+				if (doc)
+					docs.set(doc);
+				else {
+					let docNameMap = new Map();
+					docs.set(docName, docNameMap);
+				}
 			}
-			this.insertText({ collection: info.collection, document_id: info.document_id, name: info.name, position: 0, value: textValue, crud: info.crud });
-		}
-		if (info.crud != 'false') {
-			crud.updateDocument({
-				collection: info.collection,
-				document_id: info.document_id,
-				data: {[info.name]: info.value},
-				upsert: info.upsert,
-				// element: info.element,
-				namespace: info.namespace,
-				room: info.room,
-				broadcast: info.broadcast,
-				broadcast_sender: info.broadcast_sender,
-				metadata:info.metadata
-			});
-		}
-	}
-	
-	/*
-	crdt.insertText({
-		collection: 'module_activities',
-		document_id: '5e4802ce3ed96d38e71fc7e5',
-		name: 'name',
-		value: 'T',
-		position: '8',
-		attributes: {bold: true}
-	})
-	*/
-	insertText(info) {
-		try {
-			let docName = this.generateDocName(info);
-			let typeName = this.generateTypeName(info);
-			if (docName) {
-				this.docs[docName].doc.getText(typeName).insert(info['position'], info['value'], info['attributes']);
-				
-				if (info.crud != 'false') {
-					let wholestring = this.docs[docName].doc.getText(typeName).toString();
-					crud.updateDocument({
-						collection: info.collection,
-						document_id: info.document_id,
-						data: {
-							[info.name]: wholestring
+			if (docs.has(docName)) {
+				let typeName = info.name;
+				if (!docs.get(docName).has(typeName)) {
+					let typeNameMap = new Map();
+					docs.get(docName).set(typeName, typeNameMap);
+				}
+				if (docs.get(docName).has(typeName)) {
+					crud.readDocumentList({		      
+						collection: "crdtNew",
+						operator: {
+							filters: [{
+								name: 'name',
+								operator: "$eq",
+								value: [docName]
+							}]
 						},
-						upsert: info.upsert,
-						namespace: info.namespace,
-						room: info.room,
-						broadcast: info.broadcast,
-						broadcast_sender: info.broadcast_sender,
-						metadata: 'crdt-change'
+					}).then(response => {
+						let changeLog = response.data[0][typeName];
+						if (!changeLog) {
+							docs.get(docName).get(typeName).set('changeLog', changeLog);
+							checkDb(info);
+						}
+						else
+							docs.get(docName).get(typeName).set('changeLog', changeLog);
 					});
+					// let changeLog = [];
+					// 	docs.get(docName).get(typeName).set('changeLog', changeLog);
+					// 	checkDb(info);
+					let cursorMap = new Map();
+					docs.get(docName).get(typeName).set('cursors', cursorMap);
 				}
 			}
-		}
-		catch (e) {
-			console.error(e); 
-		}
-	}
-	
-	/*
-	crdt.deleteText({
-		collection: 'module_activities',
-		document_id: '5e4802ce3ed96d38e71fc7e5',
-		name: 'name',
-		position: '8',
-		length: 2,
-	})
-	*/
-	deleteText(info) {
-		try{
-			let docName = this.generateDocName(info);
-			let typeName = this.generateTypeName(info);
-			if (docName) {
-				this.docs[docName].doc.getText(typeName).delete(info['position'], info['length']);
-				
-				if (info.crud != 'false') {
-					let wholestring = this.docs[docName].doc.getText(typeName).toString();
-					crud.updateDocument({
-						collection: info.collection,
-						document_id: info.document_id,
-						data: {
-							[info.name]: wholestring
-						},
-						upsert: info.upsert,
-						namespace: info.namespace,
-						room: info.room,
-						broadcast: info.broadcast,
-						broadcast_sender: info.broadcast_sender,
-						metadata: 'crdt-change'
-					});
-				}
-			}
-		}
-		catch (e) {
-			console.error(e); 
-		}
-	}
-	
-	
-	/*
-	crdt.getText({
-		collection: 'module_activities',
-		document_id: '5e4802ce3ed96d38e71fc7e5',
-		name: 'name'
-	})
-	*/
-	getText(info) {
-		try{
-			let docName = this.generateDocName(info);
-			let typeName = this.generateTypeName(info);
-			if (docName) {
-				if (!this.docs[docName]){
-					this.init(info);
-				}				
-				return this.docs[docName].doc.getText(typeName).toString();
-			} 
-			else return "--";
-		}
-		catch (e) {
-			console.error(e); 
-			return "";
-		}
-	}
-	
 
-	/*
-	crdt.getDoc({
-		collection: 'module_activities',
-		document_id: '5e4802ce3ed96d38e71fc7e5',
-		name: 'name'
-	})
-	*/
-	getDoc(info) {
-		try{
-			let docName = this.generateDocName(info);
-			let typeName = this.generateTypeName(info);
-			if (docName) {
-				if (!this.docs[docName]){
-					this.init(info);
-				}
-				return this.docs[docName].doc.getText(typeName);
-			} 
-			else return false;
 		}
-		catch (e) {
-			console.error(e); 
-			return false;
-		}
-	}
+		return true
 
-	/* 
-	crdt.getPosition(function(data))
-	crdt.getPosition(function(data){console.log(" EScuchando ahora  ",data)})
-	*/
-	getPosition(callback){
-	if(typeof miFuncion === 'function')
-		this.changeListenAwereness(callback);
-	else
-		console.error('Callback should be a function');
+	}
+	catch (e) {
+		console.log('Invalid param', e);
 	}
 }
 
-window.Y = Y;
-const cDoc = new Y.Doc();
-let org = config.organization_Id;
-let	CoCreateCrdt = new CoCreateCRDTClass(org, cDoc);
+function insertChange(info) {
+	let docName = generateDocName(info);
+	let typeName = info.name;
+	let type = 'insert';
+	const datetime = new Date
+	if (info.legnth > 0)
+		type = 'delete';
+	let change = {
+		datetime,
+		value: info.value || '',
+		start: info.position || 0,
+		end: info.position || 0,
+		legnth: info.length || 0,
+		clientId: info.userId,
+		type
+	}
+	let changeLog = docs.get(docName).get(typeName).get('changeLog');
+	changeLog.push(change)
+		
+	crud.updateDocument({
+		collection: 'crdtNew',
+		document_id: info.document_id,
+		data: {
+			name: docName,
+			[typeName]: changeLog
+		},
+		upsert: true,
+		namespace: info.namespace,
+		room: info.room,
+		broadcast: info.broadcast,
+		broadcast_sender: info.broadcast_sender,
+		metadata: 'crdt-change'
+	});
+	
+}
 
-export default CoCreateCrdt;
+function checkDb(info) {
+	let { collection, document_id, name } = info;
+	crud.readDocument({ collection, document_id, name }).then(response => {
+		let string = response.data[name];
+		if (string) {
+			info.value = string;
+			insertChange(info)
+		}
+	});
+}
 
+/*
+crdt.replaceText({
+	collection: "module",
+	document_id: "",
+	name: "",
+	value: "",
+	updateCrud: true | false,
+	element: dom_object,
+	metadata: "xxxx"
+})
+*/
+function replaceText(info) {
+	let docName = generateDocName(info);
+	let typeName = info.name;
+	// if (info.updateCrud != false) info.updateCrud = true;
+
+	if (docName) {
+		let oldData = getText(info);
+		let textValue = info['value'].toString();
+		if (oldData && oldData.length > 0) {
+			deleteText({ collection: info.collection, document_id: info.document_id, name: info.name, position: 0, length: Math.max(oldData.length, textValue.length), crud: false });
+		}
+		insertText({ collection: info.collection, document_id: info.document_id, name: info.name, position: 0, value: textValue, crud: info.crud });
+	}
+}
+
+/*
+crdt.insertText({
+	collection: 'module_activities',
+	document_id: '5e4802ce3ed96d38e71fc7e5',
+	name: 'name',
+	value: 'T',
+	position: '8',
+	attributes: {bold: true}
+})
+*/
+function insertText(info) {
+	updateCrdt(info)
+}
+
+/*
+crdt.deleteText({
+	collection: 'module_activities',
+	document_id: '5e4802ce3ed96d38e71fc7e5',
+	name: 'name',
+	position: '8',
+	length: 2,
+})
+*/
+
+function deleteText(info) {
+	updateCrdt(info)
+}
+
+function updateCrdt(info) {
+	try {
+		let docName = generateDocName(info);
+		let typeName = info.name;
+		if (docName) {
+			if (!docs.has(docName))
+				init(info)
+			if (!docs.get(docName).has(typeName))
+				init(info)
+
+			insertChange(info)
+
+			message.send({
+				room: "",
+				emit: {
+					message: "crdt",
+					data: {
+						collection: info.collection,
+						document_id: info.document_id,
+						name: info.name,
+						eventDelta: [
+							{ retain: info['position'] },
+							{ insert: info['value'], attributes: info['attributes'] },
+							{ delete: info['length'] }
+						]
+					}
+				},
+			});
+
+			if (info.crud != 'false') {
+				let wholestring = getText(info);
+				crud.updateDocument({
+					collection: info.collection,
+					document_id: info.document_id,
+					data: {
+						[info.name]: wholestring
+					},
+					upsert: info.upsert,
+					namespace: info.namespace,
+					room: info.room,
+					broadcast: info.broadcast,
+					broadcast_sender: info.broadcast_sender,
+					metadata: 'crdt-change'
+				});
+			}
+		}
+	}
+	catch (e) {
+		console.error(e);
+	}
+
+}
+
+message.listen('crdt', function(data) {
+	const updateEvent = new CustomEvent('cocreate-crdt-update', {
+		detail: { ...data },
+	});
+
+	window.dispatchEvent(updateEvent);
+});
+
+/*
+crdt.getText({
+	collection: 'module_activities',
+	document_id: '5e4802ce3ed96d38e71fc7e5',
+	name: 'name'
+})
+*/
+
+// String.prototype.splice = function(index, del, ...newStrs) {
+// 	let str = this.split('');
+// 	str.splice(index, del, newStrs.join('') || '');
+// 	return str.join('');
+// }
+String.prototype.customSplice = function (index, absIndex, string) {
+    return this.slice(0, index) + string+ this.slice(index + Math.abs(absIndex));
+};
+
+function getText(info) {
+	try {
+		let docName = generateDocName(info);
+		let typeName = info.name;
+		if (docName) {
+			if (!docs.has(docName))
+				init(info)
+			if (!docs.get(docName).has(typeName))
+				init(info)
+		}
+		let string = "";
+		let changeLog = docs.get(docName).get(typeName).get('changeLog')
+		for (let change of changeLog) {
+			string = string.customSplice(change.start, change.legnth, change.value)
+		}
+		console.log(string)
+		return string;
+	}
+	catch (e) {
+		console.error(e);
+		return "";
+	}
+}
+
+
+/* 
+crdt.getPosition(function(data))
+crdt.getPosition(function(data){console.log(" EScuchando ahora  ",data)})
+*/
+function getPosition(callback) {
+	if (typeof miFuncion === 'function')
+		this.changeListenAwereness(callback);
+	else
+		console.error('Callback should be a function');
+}
+
+
+function sendPosition(info) {
+	try {
+		let docName = generateDocName(info);
+		let typeName = info.name;
+		let type = docs.get(docName).get(typeName);
+		let start = info.start;
+		let end = info.end;
+		let color = info.color;
+		let name = info.name;
+		info.user_id = info.user_id || "test";
+		if (!type) return;
+		if (start != null && end != null) {
+			type.get('cursors').set(info.user_id, { start, end, color, name });
+		}
+		else {
+			type.get('cursors').delete(info.user_id);
+		}
+
+		message.send({
+			room: "",
+			emit: {
+				message: "cursor",
+				data: {
+					collection: info.collection,
+					document_id: info.document_id,
+					name: info.name,
+					start,
+					end,
+					clientId: info.user_id || 'test',
+					user: {
+						'color': color,
+						'name': name
+					}
+				}
+			},
+		});
+	}
+	catch (e) {
+		console.error(e);
+	}
+}
+
+message.listen('cursor', function(selection) {
+	if (selection.start != null && selection.end != null) {
+		const cursorUpdate = new CustomEvent('updateCursor', {
+			detail: { selection },
+		});
+		window.dispatchEvent(cursorUpdate);
+	}
+	else {
+		const cursorRemove = new CustomEvent('removeCursor', {
+			detail: { clientId: selection.clientId },
+		});
+		window.dispatchEvent(cursorRemove);
+	}
+})
+
+function removeCursor(clientId, aw) {
+	const cursorRemove = new CustomEvent('removeCursor', {
+		detail: { clientId, aw },
+	});
+	window.dispatchEvent(cursorRemove);
+	return;
+
+}
+
+// function deleteDoc(docName) {
+// 	if (this.docs[docName]) {
+// 		delete this.docs[docName];
+// 	}
+// }
+
+// function destroyObserver(docName, typeName) {
+// 	this.docs[docName].doc.getText(typeName).unobserve((event) => {});
+// 	this.docs[docName].socket.awareness.off('change', this._awarenessListener);
+// }
+
+function generateDocName(info) {
+	let docName = { collection: info.collection, document_id: info.document_id };
+	return `${info.collection}${info.document_id}`;
+	// return btoa(JSON.stringify(docName));
+}
+
+export default { init, getText, insertText, deleteText, replaceText, getPosition, sendPosition };
