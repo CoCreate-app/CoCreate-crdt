435f6f3139c313277b012417323b501bbe56dae7
Frank Pagan <frank@cocrete.app>
2023-04-11 01:32:03 -0600

    fix: renamed domains to hosts

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -12,7 +12,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/crdt/index.html",
                 "src": "{{./docs/index.html}}",
-                "domains": [
+                "hosts": [
                     "*",
                     "general.cocreate.app"
                 ],
